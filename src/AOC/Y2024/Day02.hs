60d8428ab5afa885a89e7681f6b0033b4b3bcfcc
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-02 18:12:09 -0800

    Refactor safe check for part 1

--- src/AOC/Y2024/Day02.hs
+++ src/AOC/Y2024/Day02.hs
@@ -1,11 +1,12 @@
 module AOC.Y2024.Day02 (runDay) where
 
-import           Control.Arrow    ((&&&))
-import           Data.Function    (on)
-import           Data.Maybe       (fromMaybe)
-import qualified Program.RunDay   as R (Day, runDay)
-import           Text.Parsec      (char, digit, many1, newline, sepBy)
-import           Text.Parsec.Text (Parser)
+import           Control.Applicative ((<|>))
+import           Control.Arrow       ((&&&))
+import           Data.Function       (on)
+import           Data.Maybe          (fromMaybe)
+import qualified Program.RunDay      as R (Day, runDay)
+import           Text.Parsec         (char, digit, many1, newline, sepBy)
+import           Text.Parsec.Text    (Parser)
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
@@ -20,18 +21,32 @@ type OutputA = Int
 
 type OutputB = Int
 
------------- UTIL --------------
-monotonic :: Int -> Int -> Bool
-monotonic = (==) `on` (>0)
+data Dir = Inc | Dec | None
 
+instance Eq Dir where
+    (==) :: Dir -> Dir -> Bool
+    Inc == Dec = False
+    Dec == Inc = False
+    _ == _     = True
+
+------------ UTIL --------------
 withinLimits :: Int -> Bool
 withinLimits = uncurry (&&) . ((>=1) &&& (<=3)) . abs
 
-isSafe :: Maybe Int -> Maybe Int -> [Int] -> Bool
-isSafe Nothing Nothing (x:xs) = isSafe Nothing (Just x) xs
-isSafe firstInc (Just prev) (x:xs) =
-    (\d -> withinLimits d && monotonic d (fromMaybe d firstInc) && isSafe (Just d) (Just x) xs) (x - prev)
-isSafe _ _ _ = True
+intToDir :: Int -> Dir
+intToDir x | x > 0 = Inc
+           | x < 0 = Dec
+           | otherwise = None
+
+safe :: Dir -> Int -> Bool
+safe dir = (&&) <$> withinLimits <*> (dir ==) . intToDir
+
+isSafe :: Dir -> [Int] -> Bool
+isSafe dir (x1:x2:xs) = (\diff -> safe dir diff && isSafe (intToDir diff) (x2:xs)) (x2 - x1)
+isSafe _ _ = True
+
+monotonic :: Int -> Int -> Bool
+monotonic = (==) `on` (>0)
 
 isSafeAfterRemoval :: Maybe Int -> Maybe Int -> [Int] -> Bool -> Bool
 isSafeAfterRemoval Nothing Nothing (x:xs) hasFailure = isSafeAfterRemoval Nothing (Just x) xs hasFailure || isSafeAfterRemoval Nothing (Just x) xs True
@@ -41,7 +56,7 @@ isSafeAfterRemoval _ _ _ _ = True
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = length . filter id . map (isSafe Nothing Nothing)
+partA = length . filter id . map (isSafe None)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
