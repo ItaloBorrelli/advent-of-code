6556553f4b8325c812962d629b7645a1e8ca3884
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-02 23:19:12 -0800

    Solve p2 with the grace and elegance haskell is known for, thank you applicatives

--- src/AOC/Y2024/Day03.hs
+++ src/AOC/Y2024/Day03.hs
@@ -1,8 +1,5 @@
 module AOC.Y2024.Day03 (runDay) where
 
-import           Data.Functor     (($>))
-import           Data.Maybe       (catMaybes)
-import           Data.Void        (Void)
 import qualified Program.RunDay   as R (Day, runDay)
 import           Text.Parsec      (anyChar, char, digit, eof, many, many1,
                                    string, try, (<|>))
@@ -12,37 +9,46 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
-parseAnyMul :: Parser (Int, Int)
-parseAnyMul =
+parseMul :: Parser (Int, Int)
+parseMul =
     string "mul(" *>
     (((,) . read <$> many1 digit)
          <*> (char ',' *> (read <$> many1 digit)))
     <* char ')'
 
-parseAnyOrSkip :: Parser (Maybe (Int, Int))
-parseAnyOrSkip =
-    (Just <$> try parseAnyMul)
-    <|> (anyChar $> Nothing)
-
 inputParser :: Parser Input
-inputParser = catMaybes <$> (many parseAnyOrSkip <* eof)
+inputParser = many $ do
+    Mul <$> try parseMul
+    <|> (Enable <$ try (string "do()"))
+    <|> (Disable <$ try (string "don't()"))
+    <|> (Junk <$> anyChar)
 
 ------------ TYPES -------------
-type Input = [(Int, Int)]
--- type Input = [Either Char (Int, Int)]
+type Input = [Commands]
 
 type OutputA = Int
--- type OutputA = [Either Char (Int, Int)]
 
-type OutputB = Void
+type OutputB = Int
+
+data Commands = Mul (Int, Int) | Junk Char | Enable | Disable deriving (Show)
 
 ------------ UTIL --------------
+multMults :: Commands -> Int
+multMults (Mul (a, b)) = a * b
+multMults _            = 0
+
+filterMults :: Bool -> [Commands] -> [(Int, Int)]
+filterMults False (Enable:xs) = filterMults True xs
+filterMults True (Disable:xs) = filterMults False xs
+filterMults False (_:xs)      = filterMults False xs
+filterMults True (Mul x:xs)   = x : filterMults True xs
+filterMults True (_:xs)       = filterMults True xs
+filterMults _ []              = []
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = sum . map (uncurry (*))
--- partA x = x
+partA = sum . map multMults
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map (uncurry (*)) . filterMults True
