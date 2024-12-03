d509192a98d6bb85f41b0b8553e9af0689fabcad
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-02 22:30:09 -0800

    Solve p1

--- src/AOC/Y2024/Day03.hs
+++ src/AOC/Y2024/Day03.hs
@@ -1,21 +1,40 @@
 module AOC.Y2024.Day03 (runDay) where
 
+import           Data.Either      (rights)
 import           Data.Void
+import           Debug.Trace      (trace)
 import qualified Program.RunDay   as R (Day, runDay)
 import           Text.Parsec
+import           Text.Parsec      (many1, string)
 import           Text.Parsec.Text
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+parseAnyMul :: Parser (Int, Int)
+parseAnyMul = do
+    _ <- string "mul("
+    digits1 <- many1 digit
+    _ <- char ','
+    digits2 <- many1 digit
+    _ <- char ')'
+    return (read digits1, read digits2)
+
+parseAnyOrSkip :: Parser (Either Char (Int, Int))
+parseAnyOrSkip =
+    (Right <$> try parseAnyMul)
+    <|> (Left <$> anyChar)
+
 inputParser :: Parser Input
-inputParser = fail "Parser not yet implemented!"
+inputParser = rights <$> (many parseAnyOrSkip <* eof)
 
 ------------ TYPES -------------
-type Input = Void
+type Input = [(Int, Int)]
+-- type Input = [Either Char (Int, Int)]
 
-type OutputA = Void
+type OutputA = Int
+-- type OutputA = [Either Char (Int, Int)]
 
 type OutputB = Void
 
@@ -23,7 +42,8 @@ type OutputB = Void
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . map (uncurry (*))
+-- partA x = x
 
 ------------ PART B ------------
 partB :: Input -> OutputB
