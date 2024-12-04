9421fca107533a49322db8c27f290002e40c6e9f
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-03 22:54:49 -0800

    [#202404] Clean up some imports

--- src/AOC/Y2024/Day04.hs
+++ src/AOC/Y2024/Day04.hs
@@ -1,11 +1,10 @@
 module AOC.Y2024.Day04 (runDay) where
 
-import Program.RunDay qualified as R (Day, runDay)
-import Text.Parsec
-import Text.Parsec.Text
 import Data.List (transpose)
-import Data.Maybe (fromMaybe, isNothing, fromJust)
-import Debug.Trace (trace)
+import Data.Maybe (fromJust, isNothing)
+import Program.RunDay qualified as R (Day, runDay)
+import Text.Parsec (char, eof, many, newline, (<|>))
+import Text.Parsec.Text (Parser)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -33,32 +32,32 @@ type XMASNonet = (XMASTrio, XMASTrio, XMASTrio)
 ------------ UTIL --------------
 
 checkRow :: [XMAS] -> Int
-checkRow (X:M:A:S:rest) = 1 + checkRow (S:rest)
-checkRow (S:A:M:X:rest) = 1 + checkRow (X:rest)
-checkRow (_:rest) = checkRow rest
+checkRow (X : M : A : S : rest) = 1 + checkRow (S : rest)
+checkRow (S : A : M : X : rest) = 1 + checkRow (X : rest)
+checkRow (_ : rest) = checkRow rest
 checkRow _ = 0
 
 checkRow2 :: [Maybe XMAS] -> Int
-checkRow2 (Just X:Just M:Just A:Just S:rest) = 1 + checkRow2 (Just S:rest)
-checkRow2 (Just S:Just A:Just M:Just X:rest) = 1 + checkRow2 (Just X:rest)
-checkRow2 (_:rest) = checkRow2 rest
+checkRow2 (Just X : Just M : Just A : Just S : rest) = 1 + checkRow2 (Just S : rest)
+checkRow2 (Just S : Just A : Just M : Just X : rest) = 1 + checkRow2 (Just X : rest)
+checkRow2 (_ : rest) = checkRow2 rest
 checkRow2 _ = 0
 
 shiftRowForwardByN :: [Maybe XMAS] -> Int -> [Maybe XMAS]
 shiftRowForwardByN [] _ = []
 shiftRowForwardByN xs 0 = xs
-shiftRowForwardByN (_:xs) n = shiftRowForwardByN (xs ++ [Nothing]) (n - 1)
+shiftRowForwardByN (_ : xs) n = shiftRowForwardByN (xs ++ [Nothing]) (n - 1)
 
 first3 :: [XMAS] -> Maybe XMASTrio
-first3 (x:y:z:_) = Just (x, y, z)
+first3 (x : y : z : _) = Just (x, y, z)
 first3 _ = Nothing
 
 firstNonet :: [[XMAS]] -> Maybe XMASNonet
-firstNonet (x:y:z:_) = do
-    a <- first3 x
-    b <- first3 y
-    c <- first3 z
-    return (a, b, c)
+firstNonet (x : y : z : _) = do
+  a <- first3 x
+  b <- first3 y
+  c <- first3 z
+  return (a, b, c)
 firstNonet _ = Nothing
 
 checkNonet :: XMASNonet -> Bool
@@ -67,26 +66,28 @@ checkNonet _ = False
 
 moving3x3WindowAccum :: [[XMAS]] -> Int
 moving3x3WindowAccum [[], [], []] = 0
-moving3x3WindowAccum (x:xs) =
-    let non = firstNonet (x:xs)
-        good = if isNothing non then 0 else fromEnum (checkNonet (fromJust non))
-    in good + moving3x3WindowAccum (map tail (x:xs))
+moving3x3WindowAccum (x : xs) =
+  let non = firstNonet (x : xs)
+      good = if isNothing non then 0 else fromEnum (checkNonet (fromJust non))
+   in good + moving3x3WindowAccum (map tail (x : xs))
 moving3x3WindowAccum _ = 0
 
 moving3x3WindowAccum2 :: [[XMAS]] -> Int
-moving3x3WindowAccum2 (x:y:z:rest) = moving3x3WindowAccum [x, y, z] + moving3x3WindowAccum2 (y:z:rest)
+moving3x3WindowAccum2 (x : y : z : rest) = moving3x3WindowAccum [x, y, z] + moving3x3WindowAccum2 (y : z : rest)
 moving3x3WindowAccum2 _ = 0
 
 ------------ PART A ------------
 partA :: Input -> OutputA
 partA x =
-    let (m, nums) = (length x, [0..m])
-    in sum [sum (map checkRow x)
-    , sum (map checkRow (transpose x))
-    , sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (map Just) x) nums)))
-    , sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (reverse . map Just) x) nums)))
-    , sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (map Just) x) (reverse nums))))
-    , sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (reverse . map Just) x) (reverse nums))))]
+  let (m, nums) = (length x, [0 .. m])
+   in sum
+        [ sum (map checkRow x),
+          sum (map checkRow (transpose x)),
+          sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (map Just) x) nums))),
+          sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (reverse . map Just) x) nums))),
+          sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (map Just) x) (reverse nums)))),
+          sum (map checkRow2 (transpose $ (map . uncurry) shiftRowForwardByN (zip (map (reverse . map Just) x) (reverse nums))))
+        ]
 
 ------------ PART B ------------
 partB :: Input -> OutputB
