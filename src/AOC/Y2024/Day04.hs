83acd6c7f1059321ca35ffdff63f4ab854e89ae4
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-03 22:53:39 -0800

    [#202404] Solve part 2 with pretty unfortunate looking code

--- src/AOC/Y2024/Day04.hs
+++ src/AOC/Y2024/Day04.hs
@@ -4,6 +4,8 @@ import Program.RunDay qualified as R (Day, runDay)
 import Text.Parsec
 import Text.Parsec.Text
 import Data.List (transpose)
+import Data.Maybe (fromMaybe, isNothing, fromJust)
+import Debug.Trace (trace)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -24,6 +26,10 @@ type OutputB = Int
 
 data XMAS = X | M | A | S deriving (Eq, Show)
 
+type XMASTrio = (XMAS, XMAS, XMAS)
+
+type XMASNonet = (XMASTrio, XMASTrio, XMASTrio)
+
 ------------ UTIL --------------
 
 checkRow :: [XMAS] -> Int
@@ -43,6 +49,34 @@ shiftRowForwardByN [] _ = []
 shiftRowForwardByN xs 0 = xs
 shiftRowForwardByN (_:xs) n = shiftRowForwardByN (xs ++ [Nothing]) (n - 1)
 
+first3 :: [XMAS] -> Maybe XMASTrio
+first3 (x:y:z:_) = Just (x, y, z)
+first3 _ = Nothing
+
+firstNonet :: [[XMAS]] -> Maybe XMASNonet
+firstNonet (x:y:z:_) = do
+    a <- first3 x
+    b <- first3 y
+    c <- first3 z
+    return (a, b, c)
+firstNonet _ = Nothing
+
+checkNonet :: XMASNonet -> Bool
+checkNonet ((x1, _, x3), (_, A, _), (z1, _, z3)) = ((x1 == M && z3 == S) || (x1 == S && z3 == M)) && ((x3 == M && z1 == S) || (x3 == S && z1 == M))
+checkNonet _ = False
+
+moving3x3WindowAccum :: [[XMAS]] -> Int
+moving3x3WindowAccum [[], [], []] = 0
+moving3x3WindowAccum (x:xs) =
+    let non = firstNonet (x:xs)
+        good = if isNothing non then 0 else fromEnum (checkNonet (fromJust non))
+    in good + moving3x3WindowAccum (map tail (x:xs))
+moving3x3WindowAccum _ = 0
+
+moving3x3WindowAccum2 :: [[XMAS]] -> Int
+moving3x3WindowAccum2 (x:y:z:rest) = moving3x3WindowAccum [x, y, z] + moving3x3WindowAccum2 (y:z:rest)
+moving3x3WindowAccum2 _ = 0
+
 ------------ PART A ------------
 partA :: Input -> OutputA
 partA x =
@@ -56,4 +90,4 @@ partA x =
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB _ = 1
+partB = moving3x3WindowAccum2
