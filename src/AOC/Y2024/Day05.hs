3a77cf4bba8df8347390e5bef0aa87207073c81a
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-04 23:50:41 -0800

    [#202405] ormolu style and comment isPageBefore func

--- src/AOC/Y2024/Day05.hs
+++ src/AOC/Y2024/Day05.hs
@@ -1,12 +1,12 @@
 module AOC.Y2024.Day05 (runDay) where
 
-import Data.Void ( Void )
-import qualified Program.RunDay   as R (Day, runDay)
-import Text.Parsec ( char, digit, newline, endBy, many1, sepBy, eof )
-import Text.Parsec.Text ( Parser )
+import Control.Arrow (Arrow (second))
 import Data.Map.Strict (Map, fromListWith, (!?))
-import Control.Arrow (Arrow(second))
 import Data.Tuple (swap)
+import Data.Void (Void)
+import Program.RunDay qualified as R (Day, runDay)
+import Text.Parsec (char, digit, endBy, eof, many1, newline, sepBy)
+import Text.Parsec.Text (Parser)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -29,33 +29,33 @@ type OutputB = Void
 
 parseRule :: Parser OrderRule
 parseRule = do
-    ord1 <- read <$> many1 digit
-    _ <- char '|'
-    ord2 <- read <$> many1 digit
-    return (ord1, ord2)
+  ord1 <- read <$> many1 digit
+  _ <- char '|'
+  ord2 <- read <$> many1 digit
+  return (ord1, ord2)
 
 parsePages :: Parser [PageUpdate]
 parsePages = (read <$> many1 digit) `sepBy` char ',' `sepBy` newline
 
 inputParser :: Parser Input
 inputParser = do
-    rules <- parseRule `endBy` newline
-    _ <- newline
-    pages <- parsePages <* eof
-    return (rules, pages)
-
+  rules <- parseRule `endBy` newline
+  _ <- newline
+  pages <- parsePages <* eof
+  return (rules, pages)
 
 ----------- PART A&B -----------
 
 combineKeys :: [OrderRule] -> OrderMap
-combineKeys = fromListWith (++) . map (second (:[]))
+combineKeys = fromListWith (++) . map (second (: []))
 
--- is second int before the first int in ordering rules where the order rules are from the last page to the first page
+-- must query be before current in the ordering rules given m mapping an int to all pages that it MUST be after
 isPageBefore :: OrderMap -> Int -> Int -> Bool
-isPageBefore m current q  = case m !? current of
-        Nothing -> False
-        Just prevPages -> q `elem` prevPages -- if q is in prevPages then q is before current
-            || any (\p -> isPageBefore m p q) prevPages
+isPageBefore m current query = case m !? current of
+  Nothing -> False -- i.e. we don't know if there's any pages that explicitly come before this book so it's not violating current
+  Just prevPages ->
+    query `elem` prevPages -- if q is in prevPages then q is before current and therefore we fail
+      || any (\p -> isPageBefore m p query) prevPages -- but if it wasn't, we need to check if any of the prevPages lead to q
 
 areAnyPagesEarly :: OrderMap -> Int -> [Int] -> Bool
 areAnyPagesEarly _ _ [] = False
@@ -63,7 +63,7 @@ areAnyPagesEarly m x ys = any (isPageBefore m x) ys
 
 isRowGood :: OrderMap -> PageUpdate -> Bool
 isRowGood _ [] = True
-isRowGood m (x:xs) = isRowGood m xs && not (areAnyPagesEarly m x xs)
+isRowGood m (x : xs) = isRowGood m xs && not (areAnyPagesEarly m x xs)
 
 getMiddle :: PageUpdate -> Int
 getMiddle xs = xs !! (length xs `div` 2)
