3c20e8148e472afde25e54afcd2becf0d63762c9
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-05 13:37:06 -0800

    [#202405] Bird sort

--- src/AOC/Y2024/Day05.hs
+++ src/AOC/Y2024/Day05.hs
@@ -3,23 +3,25 @@ module AOC.Y2024.Day05 (runDay) where
 import Control.Arrow (Arrow (second))
 import Data.List (sortBy)
 import Data.Map.Strict (Map, fromListWith, (!?))
-import Data.Maybe (isJust, isNothing)
 import Program.RunDay qualified as R (Day, runDay)
-import Text.Parsec (char, digit, endBy, eof, many1, newline, sepBy)
+import Text.Parsec (char, digit, endBy, many1, newline, sepBy)
 import Text.Parsec.Text (Parser)
+import Data.Aviary.Birds (goldfinch, cardinal)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ----------- TYPES --------------
 
-type OrderRule = (Int, Int)
+type Rule = (Int, Int)
 
-type PageUpdate = [Int]
+type Page = [Int]
 
-type OrderMap = Map Int [Int]
+type RulesMap = Map Int [Int]
 
-type Input = ([OrderRule], [PageUpdate])
+type SortResult = (Page, Bool)
+
+type Input = ([Rule], [Page])
 
 type OutputA = Int
 
@@ -27,32 +29,24 @@ type OutputB = Int
 
 ----------- PARSER -------------
 
-parseRule :: Parser OrderRule
-parseRule = do
-  ord1 <- read <$> many1 digit
-  _ <- char '|'
-  ord2 <- read <$> many1 digit
-  return (ord1, ord2)
+parseRule :: Parser Rule
+parseRule = ((,) . read <$> many1 digit) <* char '|' <*> (read <$> many1 digit)
 
-parsePages :: Parser [PageUpdate]
-parsePages = (read <$> many1 digit) `sepBy` char ',' `sepBy` newline
+parsePages :: Parser [Page]
+parsePages = filter (not . null) <$> ((read <$> many1 digit) `sepBy` char ',' `sepBy` newline)
 
 inputParser :: Parser Input
-inputParser = do
-  rules <- parseRule `endBy` newline
-  _ <- newline
-  pages <- parsePages <* eof
-  return (rules, pages)
+inputParser = (,) <$> parseRule `endBy` newline <* newline <*> parsePages
 
 ----------- PART A&B -----------
 
-combineKeys :: [OrderRule] -> OrderMap
+combineKeys :: [Rule] -> RulesMap
 combineKeys = fromListWith (++) . map (second (: []))
 
-getMiddle :: PageUpdate -> Int
+getMiddle :: Page -> Int
 getMiddle xs = xs !! (length xs `div` 2)
 
-fixPage :: OrderMap -> PageUpdate -> PageUpdate
+fixPage :: RulesMap -> Page -> Page
 fixPage m =
   sortBy
     ( \a b ->
@@ -63,15 +57,19 @@ fixPage m =
             _ -> EQ
     )
 
-fixAndRecordDiff :: OrderMap -> PageUpdate -> (PageUpdate, Bool)
+fixAndRecordDiff :: RulesMap -> Page -> SortResult
 fixAndRecordDiff m orig = (\result -> (result, result == orig)) $ fixPage m orig
+
+avianPageSort :: [Rule] -> [Page] -> [SortResult]
+avianPageSort = map . goldfinch (cardinal fixAndRecordDiff) combineKeys
+-- (b -> c -> d) -> (a -> b) -> a -> c -> d
 
 ----------- PART A -------------
 
 partA :: Input -> OutputA
-partA (rules, pages) = sum $ map (getMiddle . fst) $ filter snd $ map (fixAndRecordDiff (combineKeys rules)) pages
+partA (rules, pages) = sum $ map (getMiddle . fst) $ filter snd $ uncurry avianPageSort (rules, pages)
 
 ----------- PART B -------------
 
 partB :: Input -> OutputB
-partB (rules, pages) = sum $ map (getMiddle . fst) $ filter (not . snd) $ map (fixAndRecordDiff (combineKeys rules)) pages
+partB (rules, pages) = sum $ map (getMiddle . fst) $ filter (not . snd) $ uncurry avianPageSort (rules, pages)
