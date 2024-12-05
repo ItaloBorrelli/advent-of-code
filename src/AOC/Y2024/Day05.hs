2d82017bf33184f695ffc12dffd4693714546af7
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-04 22:56:29 -0800

    [#202405] Test case passes, but real input does not

--- src/AOC/Y2024/Day05.hs
+++ src/AOC/Y2024/Day05.hs
@@ -1,32 +1,77 @@
 module AOC.Y2024.Day05 (runDay) where
 
-import           Data.Void
+import Data.Void ( Void )
 import qualified Program.RunDay   as R (Day, runDay)
-import           Text.Parsec
-import           Text.Parsec.Text
+import Text.Parsec ( char, digit, newline, endBy, many1, sepBy, eof )
+import Text.Parsec.Text ( Parser )
+import Data.Map.Strict (Map, fromListWith, (!?))
+import Control.Arrow (Arrow(second))
+import Data.Tuple (swap)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ----------- TYPES --------------
 
-type Input = Void
+type OrderRule = (Int, Int)
 
-type OutputA = Void
+type PageUpdate = [Int]
+
+type OrderMap = Map Int [Int]
+
+type Input = ([OrderRule], [PageUpdate])
+
+type OutputA = Int
 
 type OutputB = Void
 
 ----------- PARSER -------------
 
+parseRule :: Parser OrderRule
+parseRule = do
+    ord1 <- read <$> many1 digit
+    _ <- char '|'
+    ord2 <- read <$> many1 digit
+    return (ord1, ord2)
+
+parsePages :: Parser [PageUpdate]
+parsePages = (read <$> many1 digit) `sepBy` char ',' `sepBy` newline
+
 inputParser :: Parser Input
-inputParser = fail "Parser not yet implemented!"
+inputParser = do
+    rules <- parseRule `endBy` newline
+    _ <- newline
+    pages <- parsePages <* eof
+    return (rules, pages)
+
 
 ----------- PART A&B -----------
+
+combineKeys :: [OrderRule] -> OrderMap
+combineKeys = fromListWith (++) . map (second (:[]))
+
+-- is second int before the first int in ordering rules where the order rules are from the last page to the first page
+isPageBefore :: OrderMap -> Int -> Int -> Bool
+isPageBefore m current q  = case m !? current of
+        Nothing -> False
+        Just prevPages -> q `elem` prevPages -- if q is in prevPages then q is before current
+            || any (\p -> isPageBefore m p q) prevPages
+
+areAnyPagesEarly :: OrderMap -> Int -> [Int] -> Bool
+areAnyPagesEarly _ _ [] = False
+areAnyPagesEarly m x ys = any (isPageBefore m x) ys
+
+isRowGood :: OrderMap -> PageUpdate -> Bool
+isRowGood _ [] = True
+isRowGood m (x:xs) = isRowGood m xs && not (areAnyPagesEarly m x xs)
+
+getMiddle :: PageUpdate -> Int
+getMiddle xs = xs !! (length xs `div` 2)
 
 ----------- PART A -------------
 
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (rules, pages) = sum $ map (getMiddle . fst) $ filter snd $ zip pages (map (isRowGood (combineKeys $ map swap rules)) pages)
 
 ----------- PART B -------------
 
