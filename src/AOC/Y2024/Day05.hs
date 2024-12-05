e771c0241406101c2d2fe4d7b42ef69e6b1c122f
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-05 13:45:34 -0800

    [#202405] Split up functions more

--- src/AOC/Y2024/Day05.hs
+++ src/AOC/Y2024/Day05.hs
@@ -4,7 +4,7 @@ import Control.Arrow (Arrow (second))
 import Data.List (sortBy)
 import Data.Map.Strict (Map, fromListWith, (!?))
 import Program.RunDay qualified as R (Day, runDay)
-import Text.Parsec (char, digit, endBy, many1, newline, sepBy)
+import Text.Parsec (char, digit, endBy, many1, newline, sepBy, eof)
 import Text.Parsec.Text (Parser)
 import Data.Aviary.Birds (goldfinch, cardinal)
 
@@ -36,40 +36,38 @@ parsePages :: Parser [Page]
 parsePages = filter (not . null) <$> ((read <$> many1 digit) `sepBy` char ',' `sepBy` newline)
 
 inputParser :: Parser Input
-inputParser = (,) <$> parseRule `endBy` newline <* newline <*> parsePages
+inputParser = (,) <$> parseRule `endBy` newline <* newline <*> parsePages <* eof
 
 ----------- PART A&B -----------
 
-combineKeys :: [Rule] -> RulesMap
-combineKeys = fromListWith (++) . map (second (: []))
+toMap :: [Rule] -> RulesMap
+toMap = fromListWith (++) . map (second (: []))
 
-getMiddle :: Page -> Int
-getMiddle xs = xs !! (length xs `div` 2)
+middle :: Page -> Int
+middle xs = xs !! (length xs `div` 2)
 
-fixPage :: RulesMap -> Page -> Page
-fixPage m =
-  sortBy
-    ( \a b ->
-        case m !? a of
+pageOrder :: RulesMap -> Int -> Int -> Ordering
+pageOrder m a b = case m !? a of
           Just aNext | b `elem` aNext -> LT
           _ -> case m !? b of
             Just bNext | a `elem` bNext -> GT
             _ -> EQ
-    )
 
-fixAndRecordDiff :: RulesMap -> Page -> SortResult
-fixAndRecordDiff m orig = (\result -> (result, result == orig)) $ fixPage m orig
+sortPage :: RulesMap -> Page -> Page
+sortPage = sortBy . pageOrder 
+
+sortAndCheck :: RulesMap -> Page -> SortResult
+sortAndCheck m orig = (\result -> (result, result == orig)) $ sortPage m orig
 
 avianPageSort :: [Rule] -> [Page] -> [SortResult]
-avianPageSort = map . goldfinch (cardinal fixAndRecordDiff) combineKeys
--- (b -> c -> d) -> (a -> b) -> a -> c -> d
+avianPageSort = map . goldfinch (cardinal sortAndCheck) toMap
 
 ----------- PART A -------------
 
 partA :: Input -> OutputA
-partA (rules, pages) = sum $ map (getMiddle . fst) $ filter snd $ uncurry avianPageSort (rules, pages)
+partA = sum . map (middle . fst) . filter snd . uncurry avianPageSort
 
 ----------- PART B -------------
 
 partB :: Input -> OutputB
-partB (rules, pages) = sum $ map (getMiddle . fst) $ filter (not . snd) $ uncurry avianPageSort (rules, pages)
+partB = sum . map (middle . fst) . filter (not . snd) . uncurry avianPageSort
