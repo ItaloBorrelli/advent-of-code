e1cebfaf2249ac923f7343421f868b851fc08dc1
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-06 21:26:55 -0800

    [#202407] Both parts done

--- src/AOC/Y2024/Day07.hs
+++ src/AOC/Y2024/Day07.hs
@@ -1,64 +1,70 @@
 module AOC.Y2024.Day07 (runDay) where
 
-import           Data.Void
-import Data.Bifunctor (Bifunctor (bimap))
-import Data.List (transpose)
-import Data.Map.Strict (Map, insertWith, (!), (!?))
-import Data.Set (Set, singleton, union)
-import Data.String (IsString (..))
 import Program.RunDay qualified as R
     ( Day, runDay, Day, runDay, Day, runDay )
 import Text.Parsec
     ( char,
       eof,
-      many,
       newline,
       sepBy,
       digit,
       many1,
       space,
-      anyChar,
       char,
       digit,
-      many,
       many1,
-      string,
-      try,
       char,
       eof,
-      many,
-      newline,
-      (<|>) )
+      newline )
 import Text.Parsec.Text ( Parser, Parser, Parser, Parser )
-import Util.Util ( mapFromNestedLists, safeTake, (!!?) )
-import Text.Parsec.Char (newline)
-import Text.Parsec.Combinator ( sepEndBy, sepBy )
-import Data.Tuple.Extra (both)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ----------- TYPES --------------
 
-type Input = String
+type A = Int
+type B = [Int]
+type C = (A, B)
 
-type OutputA = Input
+type Input = [C]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ----------- PARSER -------------
 
+parseLine :: Parser C
+parseLine = do
+    a <- read <$> many1 digit <* char ':'
+    _ <- space
+    b <- (read <$> many1 digit) `sepBy` char ' '
+    return (a,b)
+
 inputParser :: Parser Input
-inputParser = fail "Parser not yet implemented!"
+inputParser = parseLine `sepBy` newline <* eof
 
 ----------- PART A&B -----------
 
+
 ----------- PART A -------------
 
+operate1 :: A -> Int -> B -> Bool
+operate1 a c [] = a == c
+operate1 a c (y:xs) = operate1 a (c * y) xs || operate1 a (c + y) xs
+
 partA :: Input -> OutputA
-partA x = x
+partA = sum . map fst . filter snd . map (\(a, xs) -> (a, operate1 a (head xs) (tail xs)))
 
 ----------- PART B -------------
 
+concatNums :: Int -> Int -> Int
+concatNums a b = read (show a ++ show b)
+
+operate2 :: A -> Int -> B -> Bool
+operate2 a c [] = a == c
+operate2 a c (y:xs) = operate2 a (c * y) xs || operate2 a (c + y) xs || operate2 a (c `concatNums` y) xs
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map fst . filter snd . map (\(a, xs) -> (a, operate2 a (head xs) (tail xs)))
