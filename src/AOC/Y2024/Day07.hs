7e7775b7cc625dd846f2f0f50cc138372da32409
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-06 22:03:43 -0800

    [#202407] Refactor

--- src/AOC/Y2024/Day07.hs
+++ src/AOC/Y2024/Day07.hs
@@ -1,33 +1,38 @@
 module AOC.Y2024.Day07 (runDay) where
 
 import Program.RunDay qualified as R
-    ( Day, runDay, Day, runDay, Day, runDay )
+  ( Day,
+    runDay,
+  )
 import Text.Parsec
-    ( char,
-      eof,
-      newline,
-      sepBy,
-      digit,
-      many1,
-      space,
-      char,
-      digit,
-      many1,
-      char,
-      eof,
-      newline )
-import Text.Parsec.Text ( Parser, Parser, Parser, Parser )
+  ( char,
+    digit,
+    eof,
+    many1,
+    newline,
+    sepBy,
+    space,
+  )
+import Text.Parsec.Text (Parser)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ----------- TYPES --------------
 
-type A = Int
-type B = [Int]
-type C = (A, B)
+-- Test
+type T = Int
 
-type Input = [C]
+-- Values
+type V = [Int]
+
+-- Line
+type L = (T, V)
+
+-- Binary operator
+type O = Int -> Int -> Int
+
+type Input = [L]
 
 type OutputA = Int
 
@@ -35,36 +40,30 @@ type OutputB = Int
 
 ----------- PARSER -------------
 
-parseLine :: Parser C
-parseLine = do
-    a <- read <$> many1 digit <* char ':'
-    _ <- space
-    b <- (read <$> many1 digit) `sepBy` char ' '
-    return (a,b)
+parseLine :: Parser L
+parseLine = (,) <$> (read <$> many1 digit <* char ':' <* space) <*> ((read <$> many1 digit) `sepBy` char ' ')
 
 inputParser :: Parser Input
 inputParser = parseLine `sepBy` newline <* eof
 
 ----------- PART A&B -----------
 
+operate :: T -> [O] -> V -> Int -> Bool
+operate t _ [] c = t == c
+operate t ops (x:xs) c = any (\op -> operate t ops xs (c `op` x)) ops
+
+runLine :: [O] -> L -> Int
+runLine ops (t,xs) = if operate t ops (tail xs) (head xs) then t else 0
 
 ----------- PART A -------------
 
-operate1 :: A -> Int -> B -> Bool
-operate1 a c [] = a == c
-operate1 a c (y:xs) = operate1 a (c * y) xs || operate1 a (c + y) xs
-
 partA :: Input -> OutputA
-partA = sum . map fst . filter snd . map (\(a, xs) -> (a, operate1 a (head xs) (tail xs)))
+partA = sum . map (runLine [(+),(*)])
 
 ----------- PART B -------------
 
-concatNums :: Int -> Int -> Int
-concatNums a b = read (show a ++ show b)
-
-operate2 :: A -> Int -> B -> Bool
-operate2 a c [] = a == c
-operate2 a c (y:xs) = operate2 a (c * y) xs || operate2 a (c + y) xs || operate2 a (c `concatNums` y) xs
+(+-+) :: Int -> Int -> Int
+(+-+) a b = read (show a ++ show b)
 
 partB :: Input -> OutputB
-partB = sum . map fst . filter snd . map (\(a, xs) -> (a, operate2 a (head xs) (tail xs)))
+partB = sum . map (runLine [(+),(*),(+-+)])
