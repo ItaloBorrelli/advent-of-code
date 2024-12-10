ab5cf56a1b961321cc63420d35e305a785eea367
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-09 20:56:29 -0800

    [#202410] Prep day 10

---
+++ src/AOC/Y2024/Day10.hs
@@ -0,0 +1,34 @@
+module AOC.Y2024.Day10 (runDay) where
+
+import           Data.Void
+import qualified Program.RunDay   as R (Day, runDay)
+import           Text.Parsec
+import           Text.Parsec.Text
+
+runDay :: R.Day
+runDay = R.runDay inputParser partA partB
+
+----------- TYPES --------------
+
+type Input = Void
+
+type OutputA = Void
+
+type OutputB = Void
+
+----------- PARSER -------------
+
+inputParser :: Parser Input
+inputParser = fail "Parser not yet implemented!"
+
+----------- PART A&B -----------
+
+----------- PART A -------------
+
+partA :: Input -> OutputA
+partA = error "Not implemented yet!"
+
+----------- PART B -------------
+
+partB :: Input -> OutputB
+partB = error "Not implemented yet!"
