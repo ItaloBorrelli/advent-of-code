6b781ae7ea416cbfb84610838a4262e3cc0a4402
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-09 23:19:36 -0800

    [#202410] Refactor

--- src/AOC/Y2024/Day10.hs
+++ src/AOC/Y2024/Day10.hs
@@ -21,7 +21,9 @@ runDay = R.runDay inputParser partA partB
 
 type C = (Int, Int)
 
-type PathFromCoord = Map C Int
+type FoundNines = Map C Bool
+
+type CToPaths = Map C Int
 
 type FullMap = Map C Int
 
@@ -47,58 +49,55 @@ down = first (+ 1)
 left = second (\x -> x - 1)
 right = second (+ 1)
 
-findPaths :: PathFromCoord -> FullMap -> Maybe C -> Int -> (PathFromCoord, Int)
-findPaths ps _ Nothing _ = (ps, 0)
-findPaths ps m (Just c) 9 = if m !? c == Just 9 then (insert c 1 ps, 1) else (ps, 0)
-findPaths ps m (Just c) v =
-  case m !? c of
-    Nothing -> (ps, 0)
-    Just val ->
-      if val /= v
-        then (ps, 0)
-        else case ps !? c of
-          Just count -> (ps, count)
-          Nothing ->
-            let v' = v + 1
-                (psUp, countUp) = findPaths ps m (Just (up c)) v'
-                (psDown, countDown) = findPaths psUp m (Just (down c)) v'
-                (psLeft, countLeft) = findPaths psDown m (Just (left c)) v'
-                (psRight, countRight) = findPaths psLeft m (Just (right c)) v'
-                total = countUp + countDown + countLeft + countRight
-             in (insert c total psRight, total)
-
-findNumRoutes :: Map C Bool -> FullMap -> Maybe C -> Int -> (Map C Bool, Int)
-findNumRoutes ps _ Nothing _ = (ps, 0)
-findNumRoutes ps m (Just c) 9 = if m !? c == Just 9 && ps !? c /= Just True then (insert c True ps, 1) else (ps, 0)
-findNumRoutes ps m (Just c) v =
-  case m !? c of
-    Nothing -> (ps, 0)
-    Just val ->
-      if val /= v
-        then (ps, 0)
-        else
-          let v' = v + 1
-              (psUp, countUp) = findNumRoutes ps m (Just (up c)) v'
-              (psDown, countDown) = findNumRoutes psUp m (Just (down c)) v'
-              (psLeft, countLeft) = findNumRoutes psDown m (Just (left c)) v'
-              (psRight, countRight) = findNumRoutes psLeft m (Just (right c)) v'
-              total = countUp + countDown + countLeft + countRight
-           in (psRight, total)
-
 ----------- PART A -------------
+
+findAccesses :: FoundNines -> FullMap -> Maybe C -> Int -> (FoundNines, Int)
+findAccesses ps _ Nothing _ = (ps, 0)
+findAccesses ps m (Just c) v =
+  case m !? c of
+    Nothing -> (ps, 0)
+    Just h
+      | h /= v -> (ps, 0)
+      | h == 9 -> if ps !? c /= Just True then (insert c True ps, 1) else (ps, 0)
+      | otherwise ->
+          foldl
+            ( \(ps', count') c' ->
+                second (count' +) $
+                  findAccesses ps' m c' (v + 1)
+            )
+            (ps, 0)
+            $ map (\d -> Just (d c)) [up, down, left, right]
 
 partA :: Input -> OutputA
 partA g =
   let m = mapFromNestedLists g
       zs = indexZeroes g
-   in foldl (\total z -> (+) total $ snd $ findNumRoutes empty m (Just z) 0) 0 zs
-
--- in snd $ foldl (\(pathMap, total) z -> second (total + ) $ findPaths pathMap m (Just z) 0) (empty, 0) zs
+   in foldl (\total z -> (+) total $ snd $ findAccesses empty m (Just z) 0) 0 zs
 
 ----------- PART B -------------
+
+findTrails :: CToPaths -> FullMap -> Maybe C -> Int -> (CToPaths, Int)
+findTrails ps _ Nothing _ = (ps, 0)
+findTrails ps m (Just c) v =
+  case m !? c of
+    Nothing -> (ps, 0)
+    Just h
+      | h /= v -> (ps, 0)
+      | h == 9 -> (insert c 1 ps, 1)
+      | otherwise -> case ps !? c of
+          Just count -> (ps, count)
+          Nothing ->
+            (\(ps', count') -> (insert c count' ps', count'))
+              $ foldl
+                ( \(ps', count') c' ->
+                    second (count' +) $
+                      findTrails ps' m c' (v + 1)
+                )
+                (ps, 0)
+              $ map (\d -> Just (d c)) [up, down, left, right]
 
 partB :: Input -> OutputB
 partB g =
   let m = mapFromNestedLists g
       zs = indexZeroes g
-   in snd $ foldl (\(pathMap, total) z -> second (total +) $ findPaths pathMap m (Just z) 0) (empty, 0) zs
+   in snd $ foldl (\(pathMap, total) z -> second (total +) $ findTrails pathMap m (Just z) 0) (empty, 0) zs
