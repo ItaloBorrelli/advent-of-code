7da09f1a00eb4aa0dcae46e7cc7c7a2e22a2f9b6
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-09 20:56:29 -0800

    [#202410] Prep day 10

--- test/Spec.hs
+++ test/Spec.hs
@@ -53,6 +53,7 @@ main :: IO ()
 main = hspec $ do
   describe "AoC" $ do
     -- Add new tests here
+    it "202410" $ do runDay ("2024", "10")
     it "202409" $ do runDay ("2024", "09")
     it "202408" $ do runDay ("2024", "08")
     it "202407" $ do runDay ("2024", "07")
