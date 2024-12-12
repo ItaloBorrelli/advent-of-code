69e11b8f3a5a315ec59f9a1028d5435949f31eec
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-12 10:12:26 -0800

    [#202412] p1

--- src/AOC/Y2024/Day12.hs
+++ src/AOC/Y2024/Day12.hs
@@ -5,7 +5,7 @@ import qualified Program.RunDay   as R (Day, runDay)
 import Text.Parsec ( newline, sepBy, eof, many )
 import Text.Parsec.Text ( Parser )
 import Text.Parsec.Char (noneOf)
-import Data.Map.Strict (Map, (!?), insertWith, foldlWithKey, empty)
+import Data.Map.Strict (Map, (!?), (!), insertWith, foldlWithKey, empty, insert, mapAccumWithKey, keys)
 import Data.Bifunctor (Bifunctor(second, first))
 import Util.Util (mapFromNestedLists)
 import qualified Data.Map.Strict as M (foldl)
@@ -28,14 +28,12 @@ type P = Int
 
 type A = Int
 
-type PA = Map Char (P, A)
-
-type M = Map C Char
+type M = Map C (Bool, Char)
 
 ----------- PARSER -------------
 
 inputParser :: Parser M
-inputParser = mapFromNestedLists <$> many (noneOf "\n") `sepBy` newline <* eof
+inputParser = mapFromNestedLists . map (map (False,)) <$> many (noneOf "\n") `sepBy` newline <* eof
 
 ----------- PART A&B -----------
 
@@ -45,23 +43,31 @@ down = first (+ 1)
 left = second (\x -> x - 1)
 right = second (+ 1)
 
-borderSame :: M -> C -> Char -> Int
-borderSame m c ch = sum $ map (fromEnum . (\d -> Just ch /= m !? d c)) [up, down, left, right]
+check :: M -> Char -> (P, [C]) -> C -> (P, [C])
+check m ch (p, n) c' = case m !? c' of
+    Nothing -> (p + 1, n)
+    Just (v, ch') -> (if ch /= ch' then p + 1 else p, if ch == ch' && not v then c':n else n)
+
+borderSame :: M -> C -> Char -> (P, [C])
+borderSame m c ch = foldl (check m ch) (0, []) $ map ($ c) [up, down, left, right]
 
 addPairs :: (Int, Int) -> (Int, Int) -> (Int, Int)
 addPairs (a, b) (c, d) = (a + c, b + d)
 
-accum :: M -> PA -> C -> Char -> PA
-accum m pa c ch =
-    let p' = borderSame m c ch
-    in insertWith addPairs ch (p', 1) pa
+checkArea :: M -> (P, A) -> C -> Char -> (M, (P, A))
+checkArea m (p, a) c ch =
+    let m' = insert c (True, ch) m
+        (p', cs) = borderSame m' c ch
+    in foldl (\(m'', (p'', a')) c' -> if not (fst (m'' ! c')) then checkArea m'' (p'', a') c' ch else (m'', (p'', a'))) (m', (p + p', a + 1)) cs
+
 
 ----------- PART A -------------
 
 partA :: Input -> OutputA
-partA m =
-    let n = foldlWithKey (accum m) (empty :: PA) m
-    in trace (show n) M.foldl (\x (p, a) -> x + (p * a)) 0 n
+partA m = fst $ foldl (\(total, m') k ->
+    let (v, ch) = (m' ! k)
+        (m'', (p, a)) = if not v then checkArea m' (0, 0) k ch else (m', (0, 0))
+    in trace (show (ch, k, p, a)) (total + (p * a), m'')) (0, m) (keys m)
 
 ----------- PART B -------------
 
