5591f727327dad83b406bfc28a1e8cdd50f0a5dd
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-13 15:02:39 -0800

    [#202406] It works, and refactor

--- src/AOC/Y2024/Day06.hs
+++ src/AOC/Y2024/Day06.hs
@@ -1,145 +1,122 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 module AOC.Y2024.Day06 (runDay) where
 
-import Data.Bifunctor (Bifunctor (bimap))
+import Data.Bifunctor (Bifunctor (first, second))
 import Data.List (transpose)
-import Data.Map.Strict (Map, insertWith, (!), (!?))
+import Data.Map.Lazy (Map, empty, insert, insertWith, member, (!?))
 import Data.Set (Set, singleton, union)
-import Data.String (IsString (..))
 import Program.RunDay qualified as R (Day, runDay)
-import Text.Parsec (char, eof, many, newline, sepBy, (<|>))
+import Text.Parsec (char, eof, many1, newline, sepBy, (<|>))
 import Text.Parsec.Text (Parser)
 import Util.Util (mapFromNestedLists)
+import Data.Maybe (fromMaybe)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ----------- TYPES --------------
 
-type Input = (V, M)
+-- Coordinate
+type C = (Int, Int)
+
+-- Direction
+data D = U | D | L | R deriving (Eq, Ord, Show)
+
+-- Guard
+type G = (C, D)
+
+-- O: Obstruction
+-- N: Empty
+data Spot = O | N deriving (Eq, Show)
+
+data Start = Start Spot | Caret deriving (Eq)
+
+type Grid = [[Start]]
+
+-- Map of Coordinates to Spots
+type M = Map C Spot
+
+-- Map of Coordinates to a set of Directions
+type V = Map C (Set D)
+
+type Input = (G, M)
 
 type OutputA = Int
 
 type OutputB = Int
 
--- Coordinate
-type C = (Int, Int)
-
--- Step
-type S = C
-
--- Direction
-data D = Up | Do | Le | Ri deriving (Eq, Ord, Show)
-
--- Vector
-type V = (C, D)
-
--- Vi: Visited
--- Ob: Obstruction
--- Em: Empty
-data Spot = Vi (Set D) | Ob | Em | St deriving (Eq)
-
--- Map of Coordinates to Spots
-type M = Map C Spot
-
-instance Show Spot where
-  show :: Spot -> String
-  show (Vi _) = "X"
-  show Ob = "#"
-  show Em = "."
-  show St = "X"
-
-instance IsString Spot where
-  fromString "X" = Vi (singleton Up)
-  fromString "#" = Ob
-  fromString "." = Em
-  fromString _ = error "Invalid Spot string"
-
 ----------- PARSER -------------
 
-getStart :: [[Spot]] -> [(Int, Int)]
-getStart = concatMap (\(y, row) -> [(x, y) | (x, spot) <- zip [0 ..] row, spot == St]) . zip [0 ..]
+start :: Grid -> G
+start = head . concatMap (\(y, row) -> [((x, y), U) | (x, spot) <- zip [0 ..] row, spot == Caret]) . zip [0 ..]
 
-parseLine :: Parser [Spot]
-parseLine = many ((St <$ char '^') <|> (Em <$ char '.') <|> (Ob <$ char '#'))
-
-combine :: Spot -> Spot -> Spot
-combine (Vi a) (Vi b) = Vi (a `union` b)
-combine (Vi a) _ = Vi a
-combine _ old = old
-
-vecsert :: V -> M -> M
-vecsert (c, d) = insertWith combine c (Vi $ singleton d)
-
-formInput :: [[Spot]] -> Input
-formInput g =
-  let (v, m) = ((head $ getStart g, Up), mapFromNestedLists $ transpose g)
-   in (v, vecsert v m)
+toMap :: Grid -> M
+toMap = mapFromNestedLists . transpose . map (map (\s -> case s of Start spot -> spot; Caret -> N))
 
 inputParser :: Parser Input
 inputParser =
-  (formInput . filter (not . null) <$> (parseLine `sepBy` newline)) <* eof
+  (\x -> (start x, toMap x))
+    <$> many1 (Caret <$ char '^' <|> Start O <$ char '#' <|> Start N <$ char '.') `sepBy` newline
+    <* eof
 
 ----------- PART A&B -----------
 
-move :: D -> S
-move Up = (0, -1)
-move Do = (0, 1)
-move Ri = (1, 0)
-move Le = (-1, 0)
-
 turn :: D -> D
-turn Le = Up
-turn Up = Ri
-turn Ri = Do
-turn Do = Le
+turn D = L
+turn L = U
+turn U = R
+turn R = D
 
-nextStep :: V -> C
-nextStep (c, d) = bimap (+ fst c) (+ snd c) (move d)
+move :: G -> C
+move (c, D) = second (1 +) c
+move (c, L) = first (flip (-) 1) c
+move (c, U) = second (flip (-) 1) c
+move (c, R) = first (1 +) c
 
-whereTo :: V -> M -> Maybe V
-whereTo (c, d) m =
-  let c' = nextStep (c, d)
-   in case m !? c' of
-        Nothing -> Nothing
-        Just Ob -> Just (c, turn d)
-        Just _ -> Just (c', d)
+moveOrTurn :: M -> G -> Maybe G
+moveOrTurn m g = case m !? move g of
+  Nothing -> Nothing
+  Just O -> Just (second turn g)
+  Just N -> Just (move g, snd g)
 
-findNextObstruction :: V -> M -> Maybe C
-findNextObstruction (c, d) m =
-  let c' = nextStep (c, d)
-   in case m !? c' of
-        Nothing -> Nothing
-        Just Ob -> Just c
-        _ -> findNextObstruction (c', d) m
+vecsert :: G -> V -> V
+vecsert (c, d) = insertWith union c (singleton d)
 
-checkVec :: V -> M -> Bool
-checkVec (c, d) m = case m ! c of
-  Vi ds -> d `elem` ds
-  _ -> False
-
-willLoop :: V -> M -> Bool
-willLoop (c, d) m =
-  case findNextObstruction (c, d) m of
-    Nothing -> False
-    Just c' -> checkVec (c', turn d) m || willLoop (c', turn d) (vecsert (c, d) m)
-
-gallivant :: V -> M -> (Int, Int)
-gallivant (c, d) m =
-  let isNew = (\case Just (Vi _) -> 0; Nothing -> 0; _ -> 1) (m !? c)
-      v' = whereTo (c, d) m
-      m' = vecsert (c, d) m
-   in case v' of
-        Nothing -> (isNew, 0)
-        Just v'' ->
-          let loops = fromEnum $ willLoop (c, turn d) (vecsert v'' m)
-           in bimap (isNew +) (loops +) (gallivant v'' m')
+isNew :: C -> V -> Bool
+isNew c = not . member c
 
 ----------- PART A -------------
 
+visit :: G -> V -> Int
+visit (c, _) v = fromEnum (isNew c v)
+
+patrol' :: G -> V -> M -> Int
+patrol' g v m = visit g v + case moveOrTurn m g of
+  Nothing -> 0
+  Just g' -> patrol' g' (vecsert g v) m
+
 partA :: Input -> OutputA
-partA = (1 +) . fst . uncurry gallivant
+partA (g, m) = patrol' g empty m
 
 ----------- PART B -------------
 
+isRepeat :: G -> V -> Bool
+isRepeat (c, d) v = fromMaybe False (v !? c >>= \ds -> Just (d `elem` ds))
+
+loopCheck :: G -> V -> M -> Bool
+loopCheck g v m = case moveOrTurn m g of
+  Nothing -> False
+  Just g' -> isRepeat g' v || loopCheck g' (vecsert g v) m
+
+tryLoop :: G -> G -> V -> M -> Int
+tryLoop g (c', _) v m = fromEnum (isNew c' v && loopCheck g v (insert c' O m))
+
+gallivant :: G -> V -> M -> Int
+gallivant g v m = case moveOrTurn m g of
+  Nothing -> 0
+  Just g' -> tryLoop g g' v m + gallivant g' (vecsert g v) m
+
 partB :: Input -> OutputB
-partB = snd . uncurry gallivant
+partB (g, m) = gallivant g empty m
