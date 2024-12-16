36a23f6f709f8ba64ed18a494913f98ede2c473b
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-16 09:53:01 -0800

    [#202416] Solved p1! Except it's high by 1000, or high by 2000 if in a dead end

--- src/AOC/Y2024/Day16.hs
+++ src/AOC/Y2024/Day16.hs
@@ -1,18 +1,19 @@
 module AOC.Y2024.Day16 (runDay) where
 
+import Data.Bifunctor (first, second)
+import Data.Heap (MinPrioHeap)
+import Data.Heap qualified as H
 import Data.Map.Strict (Map, (!?))
-import Data.Map.Strict qualified as HM
-import qualified Data.Heap as H
-import Data.Set qualified as HS
-import Data.Maybe (fromMaybe, fromJust)
+import Data.Map.Strict qualified as M
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Set (Set)
+import Data.Set qualified as S
 import Data.Void (Void)
+import Debug.Trace (trace)
 import Program.RunDay qualified as R (Day, runDay)
 import Text.Parsec (char, eof, many, newline, sepBy, (<|>))
 import Text.Parsec.Text (Parser)
 import Util.Util (mapFromNestedLists')
-import Data.Set (Set)
-import Data.Heap (MinPrioHeap)
-import Data.Bifunctor (first, second)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -68,54 +69,62 @@ inputParser = (\g -> (start g, end g, toMap g)) <$> parseMapLine `sepBy` newline
 instance (Ord a) => Ord (Distance a) where
     (<=) :: Distance a -> Distance a -> Bool
     Infinity <= Infinity = True
-    Infinity <= Dist x = False
-    Dist x <= Infinity = True
+    Infinity <= Dist _ = False
+    Dist _ <= Infinity = True
     Dist x <= Dist y = x <= y
 
 addDist :: (Num a) => Distance a -> Distance a -> Distance a
 addDist (Dist x) (Dist y) = Dist (x + y)
 addDist _ _ = Infinity
 
-(!??) :: (Eq k, Ord k) => Map k (Distance d) -> k -> Distance d
+(!??) :: (Ord k) => Map k (Distance d) -> k -> Distance d
 (!??) distanceMap key = fromMaybe Infinity (distanceMap !? key)
 
-type M' = Map (C, D) [((C, D), Int)]
+type V = (C, D)
+
+type M' = Map V [(V, Int)]
+
 newtype Graph = Graph
-   { edges :: M' } deriving (Show)
+    {edges :: M'}
+    deriving (Show)
 
 data DijkstraState = DijkstraState
-  { visitedSet :: Set (C, D)
-  , distanceMap :: Map (C, D) (Distance Int)
-  , nodeQueue :: MinPrioHeap (Distance Int) (C, D)
-  }
+    { visitedSet :: Set V
+    , distanceMap :: Map V (Distance Int)
+    , nodeQueue :: MinPrioHeap (Distance Int) V
+    }
 
-
-findShortestDistance :: Graph -> (C, D) -> (C, D) -> Distance Int
-findShortestDistance graph src dest = processQueue initialState !?? dest
+findShortestDistance :: Graph -> V -> Map V (Distance Int)
+findShortestDistance graph src = processQueue initialState
   where
-    initialVisited = HS.empty
-    initialDistances = HM.singleton src (Dist 0)
+    initialVisited = S.empty
+    initialDistances = M.singleton src (Dist 0)
     initialQueue = H.fromList [(Dist 0, src)]
     initialState = DijkstraState initialVisited initialDistances initialQueue
 
-    processQueue :: DijkstraState -> Map (C, D) (Distance Int)
+    processQueue :: DijkstraState -> Map V (Distance Int)
     processQueue ds@(DijkstraState v0 d0 q0) = case H.view q0 of
-      Nothing -> d0
-      Just ((minDist, node), q1) -> if node == dest then d0
-        else if HS.member node v0 then processQueue (ds {nodeQueue = q1})
-        else
-          -- Update the visited set
-          let v1 = HS.insert node v0
-          -- Get all unvisited neighbors of our current node
-              allNeighbors = fromMaybe [] (HM.lookup node (edges graph))
-              unvisitedNeighbors = filter (\(n, _) -> not (HS.member n v1)) allNeighbors
-          -- Fold each neighbor and recursively process the queue
-          in  processQueue $ foldl (foldNeighbor node) (DijkstraState v1 d0 q1) unvisitedNeighbors
+        Nothing -> d0
+        Just ((_, node), q1) ->
+            if S.member node v0
+                then processQueue (ds {nodeQueue = q1})
+                else -- Update the visited set
+
+                    let
+                        v1 = S.insert node v0
+                        -- Get all unvisited neighbors of our current node
+                        allNeighbors = fromMaybe [] (M.lookup node (edges graph))
+                        unvisitedNeighbors = filter (\(n, _) -> not (S.member n v1)) allNeighbors
+                     in
+                        -- Fold each neighbor and recursively process the queue
+                        processQueue $ foldl (foldNeighbor node) (DijkstraState v1 d0 q1) unvisitedNeighbors
     foldNeighbor current ds@(DijkstraState v1 d0 q1) (neighborNode, cost) =
-      let altDistance = addDist (d0 !?? current) (Dist cost)
-      in  if altDistance < d0 !?? neighborNode
-            then DijkstraState v1 (HM.insert neighborNode altDistance d0) (H.insert (altDistance, neighborNode) q1)
-            else ds
+        let
+            altDistance = addDist (d0 !?? current) (Dist cost)
+         in
+            if altDistance < d0 !?? neighborNode
+                then DijkstraState v1 (M.insert neighborNode altDistance d0) (H.insert (altDistance, neighborNode) q1)
+                else ds
 
 step :: D -> C -> C
 step U = second (\x -> x - 1)
@@ -129,28 +138,30 @@ opposite D = U
 opposite L = R
 opposite R = L
 
-makeEdges :: C -> D -> [D] -> [((C, D), ((C, D), Int))]
+makeEdges :: C -> D -> [D] -> [(V, (V, Int))]
 makeEdges _ _ [] = []
-makeEdges c dirFrom (x:xs)
-  | dirFrom == x = (from, (to, 2001)):rest
-  | dirFrom == opposite x = (from, (to, 1)):rest
-  | otherwise = (from, (to, 1001)):rest
-  where from = (c, opposite dirFrom)
-        to = (step x c, x)
-        rest = makeEdges c dirFrom xs
+makeEdges c dirFrom (x : xs)
+    | dirFrom == x = (from, (to, 2001)) : rest
+    | dirFrom == opposite x = (from, (to, 1)) : rest
+    | otherwise = (from, (to, 1001)) : rest
+  where
+    from = (c, opposite dirFrom)
+    to = (step x c, x)
+    rest = makeEdges c dirFrom xs
 
 constructEdges :: M -> C -> M' -> M'
 constructEdges m c prev = foldl insertEdge prev moreEdges
-  where 
+  where
     adjs = map fromJust $ filter (/= Nothing) $ map (\d -> let c' = step d c in case m !? c' of Just N -> Just d; _ -> Nothing) [U, R, L, D]
     moreEdges = foldl1 (++) $ map (\v -> makeEdges c v adjs) adjs
-    insertEdge acc (k, v) = HM.insertWith (++) k [v] acc
+    insertEdge acc (k, v) = M.insertWith (++) k [v] acc
 
 ----------- PART A -------------
 
 partA :: Input -> Distance Int
-partA (r, e, m) = (\g -> findShortestDistance g (r, U) (e, U)) $ Graph $ foldl (\m' k -> case m !? k of Just N -> constructEdges m k m'; _ -> m') HM.empty ks
-  where ks = HM.keys m
+partA (r, e, m) = (\m' -> minimum $ map (\d -> m' !?? (e, d)) [U, D, L, R]) $ (\g -> findShortestDistance g (r, U)) $ Graph $ foldl (\m' k -> case m !? k of Just N -> constructEdges m k m'; _ -> m') M.empty ks
+  where
+    ks = M.keys m
 
 ----------- PART B -------------
 
