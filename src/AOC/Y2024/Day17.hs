48907772dfda7bddd187bfbbf014f41c2d645d58
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-12-18 13:53:06 -0800

    [#202417] Part 2 (limited solution

--- src/AOC/Y2024/Day17.hs
+++ src/AOC/Y2024/Day17.hs
@@ -1,9 +1,10 @@
 module AOC.Y2024.Day17 (runDay) where
 
-import Data.Bits (xor)
+import Data.Bits (Bits ((.&.), (.|.)), shiftL, shiftR, xor)
 import Data.List (intercalate)
 import Data.List.Extra ((!?))
-import Data.Void (Void)
+import Data.Maybe (isNothing)
+import Data.Set (Set, empty, insert)
 import Debug.Trace (trace)
 import Program.RunDay qualified as R (Day, runDay)
 import Text.Parsec (char, digit, sepBy, string)
@@ -22,7 +23,7 @@ type Input = (R, P)
 
 type OutputA = String
 
-type OutputB = Void
+type OutputB = Maybe Int
 
 ----------- PARSER -------------
 
@@ -50,7 +51,7 @@ type Operand = Int
 
 type Ops = (Opcode, Operand)
 
-data R = R {a :: Int, b :: Int, c :: Int} deriving (Show)
+data R = R {a :: Int, b :: Int, c :: Int} deriving (Eq, Show, Ord)
 
 type Ptr = Int
 
@@ -65,7 +66,7 @@ getCombo 6 (R _ _ c) = c
 getCombo o _ = o -- 7 or higher should never exist as an operand
 
 runProgram :: Ops -> In -> Either Out (Out, Int)
-runProgram (op, l) (r@(R a b c), p) = trace (show (op, l)) $ case op of
+runProgram (op, l) (r@(R a b c), p) = case op of
     0 -> Left adv
     1 -> Left bxl
     2 -> Left bst
@@ -77,11 +78,11 @@ runProgram (op, l) (r@(R a b c), p) = trace (show (op, l)) $ case op of
   where
     combo = getCombo l r
     p' = p + 2
-    q = a `div` (2 ^ combo)
+    q = a `shiftR` combo
     adv, bxl, bst, jnz, bxc, bdv, cdv :: Out
     adv = (R q b c, p')
     bxl = (R a (b `xor` l) c, p')
-    bst = (R a (combo `mod` 8) c, p')
+    bst = (R a (combo .&. 7) c, p')
     jnz = case a of
         0 -> (r, p')
         _ -> (r, l)
@@ -89,7 +90,7 @@ runProgram (op, l) (r@(R a b c), p) = trace (show (op, l)) $ case op of
     bdv = (R a q c, p')
     cdv = (R a b q, p')
     out :: (Out, Int)
-    out = ((r, p'), combo `mod` 8)
+    out = ((r, p'), combo .&. 7)
 
 ----------- PART A -------------
 
@@ -108,5 +109,34 @@ partA (initReg, prog) = intercalate "," $ map show $ run (initReg, 0)
 
 ----------- PART B -------------
 
+chunks :: [Int]
+chunks = [0 .. 7]
+
+combineA :: Int -> Int -> Int
+combineA a x = (a `shiftL` 3) .|. x
+
+isValid :: Int -> Int -> Bool
+isValid a outExpected = outActual == outExpected
+  where
+    b' = (a .&. 7) `xor` 1
+    outActual = ((b' `xor` 5) `xor` (a `shiftR` b')) .&. 7
+
+formulateA :: [Int] -> Int -> Maybe Int
+formulateA [] a = Just a
+formulateA prog a =
+    let
+        testValues = map (combineA a) chunks
+     in
+        check testValues (head prog)
+  where
+    check :: [Int] -> Int -> Maybe Int
+    check [] _ = Nothing
+    check (t : ts) out =
+        if isValid t out
+            then case formulateA (tail prog) t of
+                Nothing -> check ts out
+                x -> x
+            else check ts out
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (_, prog) = formulateA (reverse prog) 0
