80f148f0704685c24c06d86686adbc98b35ea35f
ItaloBorrelli <iborrelli@protonmail.com>
2024-12-19 17:06:29 -0800

    [#202419] Almost fixed p2

--- src/AOC/Y2024/Day19.hs
+++ src/AOC/Y2024/Day19.hs
@@ -8,6 +8,8 @@ import Text.Parsec.Char (anyChar, satisfy)
 import Text.Parsec (newline)
 import Data.List (stripPrefix)
 import Debug.Trace (trace)
+import Data.IntMap.Lazy qualified as M
+import Data.IntMap.Lazy (IntMap)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -39,15 +41,29 @@ testPattern :: [T] -> [T] -> P -> Int
 testPattern _ _ [] = 1
 testPattern _ [] _ = 0
 testPattern allTs (t:ts) p = case stripPrefix t p of
-    Nothing -> testPattern allTs ts p 
-    Just rest -> testPattern allTs allTs rest + testPattern allTs ts p 
+    Nothing -> testPattern allTs ts p
+    Just rest -> testPattern allTs allTs rest + testPattern allTs ts p
 
 testPattern' :: [T] -> [T] -> P -> Bool
 testPattern' _ _ [] = True
 testPattern' _ [] _ = False
 testPattern' allTs (t:ts) p = case stripPrefix t p of
     Nothing -> testPattern' allTs ts p
-    Just rest -> testPattern' allTs allTs rest || testPattern' allTs ts p 
+    Just rest -> testPattern' allTs allTs rest || testPattern' allTs ts p
+
+type M = IntMap Int
+
+count :: [T] -> P -> Int
+count ts p = mFinal M.! (length p + 1)
+    where
+        mFinal = foldl foldTowel (M.empty :: M) [0..(length p)]
+        foldTowel :: M -> Int -> M
+        foldTowel m idx = foldl (\m' f -> f m') m $ map (\t -> if True then M.insertWith (+) (idx + length t) 1 else id) ts
+            where
+                x :: T -> (M -> M) -- TODO get pattern suffix to check
+                x t = case stripPrefix t p of
+                    Nothing -> id
+                    _ -> M.insertWith (+) (idx + length t) 1
 
 ----------- PART A -------------
 
