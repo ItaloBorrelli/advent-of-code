8922a3d0a885aa3447a0a0ae522bc6e17b790999
Italo Borrelli <iborrelli@oceannetworks.ca>
2025-12-02 12:53:05 -0800

    [#202501] Get day 1 part 2 to work

--- src/AOC/Y2025/Day01.hs
+++ src/AOC/Y2025/Day01.hs
@@ -1,7 +1,5 @@
 module AOC.Y2025.Day01 (runDay) where
 
-import Data.Void
-import Debug.Trace (trace)
 import Program.RunDay qualified as R (Day, runDay)
 import Text.Parsec
 import Text.Parsec.Text
@@ -50,17 +48,22 @@ partA = fst . foldl doMove (0, 50)
 ----------- PART B -------------
 
 doMove2 :: (Int, Int) -> Move -> (Int, Int)
-doMove2 (x, pos) (L, n) =
-    let
-        new_pos = (pos - n)
-        y = (pos - n) `mod` 100
-     in
-        if new_pos < 0 then (x + ((new_pos `div` 100) * (-1)) - (if pos == 0 then 1 else 0), y) else if new_pos == 0 then (x + 1, y) else (x, y)
+doMove2 (x, pos) (L, n)
+    | n == 0 = (x, pos)
+    | otherwise =
+        let
+            new_pos = (pos - n)
+            y = (pos - n) `mod` 100
+         in
+            if pos == 0
+                then (x + ((((new_pos - 1) `div` 100) * (-1)) - 1), y)
+                else if (pos - n) <= 0 then doMove2 (x + 1, 0) (L, n - pos) else (x, pos - n)
 doMove2 (x, pos) (R, n) =
     let
         new_pos = (pos + n)
+        y = (pos + n) `mod` 100
      in
-        if new_pos >= 100 then (x + (new_pos `div` 100), new_pos `mod` 100) else (x, new_pos `mod` 100)
+        if new_pos >= 100 then (x + (new_pos `div` 100), y) else (x, y)
 
 partB :: Input -> OutputB
 partB = fst . foldl doMove2 (0, 50)
