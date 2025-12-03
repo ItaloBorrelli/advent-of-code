95b6a683a8ac4c14a5d979615aa9f89b9d1355da
Italo Borrelli <iborrelli@oceannetworks.ca>
2025-12-03 10:44:53 -0800

    Day 3 2025 adjusted

--- src/AOC/Y2025/Day03.hs
+++ src/AOC/Y2025/Day03.hs
@@ -29,39 +29,26 @@ inputParser = many1 (digitToInt <$> digit) `sepEndBy` newline
 
 ----------- PART A&B -----------
 
------------ PART A -------------
-
-maxOutA :: (B, K) -> K -> (B, K)
-maxOutA y [] = y
-maxOutA y [_] = y
-maxOutA (m, _) (x : xs)
-    | x > m = if newBest > x then best else (x, xs)
-    | otherwise = best
-  where
-    best = maxOutA (x, xs) xs
-    (newBest, _) = best
-
-ff :: K -> (B, K)
-ff xs = maxOutA (0, xs) xs
-
-partA :: Input -> OutputA
-partA = sum . map (fst . (\xs -> foldl ffs (0, xs) [2, 1]))
-
------------ PART B -------------
-
-ffs :: (B, K) -> Int -> (B, K)
-ffs (total, xs) n = (\(best, remaining) -> (total + best * 10 ^ (n - 1), remaining)) $ maxOutB n (0, xs) xs
-
-maxOutB :: Int -> (B, K) -> K -> (B, K)
-maxOutB _ y [] = y
-maxOutB n (m, ms) (x : xs)
+maxOut :: Int -> (B, K) -> K -> (B, K)
+maxOut _ y [] = y
+maxOut n (m, ms) (x : xs)
     | length xs < n && x > m = (x, xs)
     | length xs < n = (m, ms)
     | x > m = if newBest > x then best else (x, xs)
     | otherwise = best
   where
-    best = maxOutB n (x, xs) xs
+    best = maxOut n (x, xs) xs
     (newBest, _) = best
+
+ffs :: (B, K) -> Int -> (B, K)
+ffs (total, xs) n = (\(best, remaining) -> (total + best * 10 ^ (n - 1), remaining)) $ maxOut n (0, xs) xs
+
+----------- PART A -------------
+
+partA :: Input -> OutputA
+partA = sum . map (fst . (\xs -> foldl ffs (0, xs) [2, 1]))
+
+----------- PART B -------------
 
 partB :: Input -> OutputB
 partB = sum . map (fst . (\xs -> foldl ffs (0, xs) [12, 11 .. 1]))
